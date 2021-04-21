4f7637dbd88ad8a450f89cb8774b58d950e4c675
Fernando <fer.oliveeira@hotmail.com>
2021-04-21 18:49:50 -0300

    feat: TypeOrm, migrations, entities and repository

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,11 @@
 import express from "express";
-
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-/**
- * Route Methods:
- * GET = Search
- * POST = Create
- * PUT = Update
- * DELETE = Delete
- * PATCH = Update specific data
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW05"
-  });
-});
-
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!"
-  })
-});
-
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
